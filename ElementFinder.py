94d26ea9de1adea58aabb98b53697cd617eb98ec
Rishabh Karwa <rishabh.karwa@pendo.io>
2022-07-08 11:26:12 -0400

    c

--- ElementFinder.py
+++ ElementFinder.py
@@ -7,14 +7,25 @@ def findElement(dir, tag, depth, attributes_type, attributes_name, text, contain
     tags_ret, depths_ret, texts_ret, attributes_ret, full_tags_ret = [], [], [], [], []
     full_attribute = attributes_type + '=' + attributes_name
     for i in range(len(tag_names)):
-        if ((tag == '' or tag == tag_names[i]) and (depth == '' or depth == tag_depths[i]) and ((attributes_type == '' and attributes_name == '') 
-        or (full_attribute in tag_attributes[i])) and (text == '' or text in tag_texts[i]) and (contains == '' or contains in tag_full_tag)):
+        if ((tag == '' or tag == str(tag_names[i])) and (depth == '' or depth == str(tag_depths[i])) and ((attributes_type == '' and attributes_name == '') or (full_attribute in tag_attributes[i])) 
+            and (text == '' or text in str(tag_texts[i])) and (contains == '' or contains in str(tag_full_tag[i]))):
             tags_ret.append(tag_names[i])
             depths_ret.append(tag_depths[i])
             texts_ret.append(tag_texts[i])
             attributes_ret.append(tag_attributes[i])
             full_tags_ret.append(tag_full_tag[i])
-    matched_data = {'Tag': tags_ret, 'Depth': depths_ret, 'Inner Text': texts_ret, 'Attribute': attributes_ret, 'Full Tag': full_tags_ret}
+    if tag == '':
+        tag = 'all'
+    if depth == '':
+        depth = 'all'
+    if text == '':
+        text = 'all'
+    if attributes_name == '' and attributes_type == '':
+        full_attribute = 'all'
+    if contains == '':
+        contains = 'all'
+    matched_data = {'Tag: ' + tag: tags_ret, 'Depth: ' + depth: depths_ret, 'Inner Text: ' + text: texts_ret, 
+        'Attribute: ' + full_attribute: attributes_ret, 'Full Tag: ' + contains: full_tags_ret}
     df = pd.DataFrame(matched_data)
     outname = 'elements_searched.csv'
     fullname = os.path.join(dir, outname)
@@ -40,7 +51,7 @@ while True:
     depths = input("""Enter the tag depth (Press 'Enter' for all depths): """)
     attributes_types = input("""Enter the attribute type (Press 'Enter' for all attribute types): """)
     attributes_names = input("""Enter the attribute name (Press 'Enter' for all attribute names): """)
-    texts = input("""Enter inner text (Press 'Enter' for all inner text): """)   
+    texts = input("""Enter inner text the tag should contain (Press 'Enter' for all inner text): """)   
     contains = input("""Enter any text the tag should contain (Press 'Enter' for all text): """)
 
     findElement(dir, tags, depths, attributes_types , attributes_names, texts, contains, tag_names, tag_depths, tag_texts, tag_attributes, tag_full_tag)
