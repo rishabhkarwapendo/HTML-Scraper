434c247cc6c5952fe327a4369f5f73804c44b08a
Rishabh Karwa <rishabh.karwa@pendo.io>
2022-07-20 11:24:17 -0400

    url exe fixed!

--- URLAnalyzer.py
+++ URLAnalyzer.py
@@ -1,18 +1,16 @@
-#separate script to check URLs and their info
+#independent script to check URLs and their info
 import re
 import validators
-from furl import furl
 import os
 import pandas as pd
 import sys
+from urllib.parse import parse_qs, urlparse
 
-urls = []
-urlKey = []
-urlValue = []
+
+urls, urlKey, urlValue = [], [], []
 specialChars = {'*' : 0, '\'' : 0, '(' : 0, ')': 0,  ';': 0, ':' : 0,  '@' :0,  '&' : 0,  '=' : 0,  '+': 0,  '$': 0,  ',' : 0,  '/': 0,  '?' : 0,  '%' : 0,  '#' : 0,  '[': 0,  ']': 0}
-query = 'False'
-fragment_identifier = ''
-port = ""
+query, fragment_identifier, port, scheme, netloc, path, username, password, hostname = '', '', '', '', '', '', '', '', ''
+
 
 #gets the depth of the url through validating substring urls
 def urlDepth(url):
@@ -22,39 +20,46 @@ def urlDepth(url):
             urls.append(url[0:s])
     urls.append(url)
 
+
 #gets all key value pairs that exists within the url
-def urlKeyValuePairs(url):
-    f = furl(url)
-    for k in f.args.keys():
-        urlKey.append(k)
-    for v in f.args.values():
-        urlValue.append(v)
+def fullAnalyzer(url):
+    global scheme
+    global netloc
+    global path
+    global query
+    global fragment_identifier
+    global username
+    global password
+    global hostname
+    global port
+    o = urlparse(url)
+    #populate the fields
+    scheme = o.scheme
+    netloc = o.netloc
+    path = o.path
+    query = o.query
+    fragment_identifier = o.fragment
+    username = o.username
+    password = o.password
+    hostname = o.hostname
+    port = o.port
+    #populate all key value parameters
+    o = parse_qs(o.query)
+    for key, value in o.items():
+        for v in value:
+            urlKey.append(key)
+            urlValue.append(v)
+    #accumulate special character count
+    for i in range(len(url)):
+        if url[i] in specialChars:
+            specialChars[url[i]] += 1
+
+    
 
 #simple regex that can be run on a url
 def urlRegex(url, regex):
     return re.search(regex, url)
 
-#get number of special character in the url
-def urlSpecialCharacters(url):
-    global query
-    global fragment_identifier
-    global port
-    if (url.find('?') != -1):
-        query = 'True'
-    if (url.find('#') != -1):
-        fragment_identifier = url[url.index('#'):]
-    for i in range(len(url)):
-        if url[i] == ':' and port == '':
-            index = i + 1
-            while index < len(url):
-                if url[index].isdigit():
-                    port += url[index]
-                else: break
-                index += 1
-    for i in range(len(url)):
-        if url[i] in specialChars:
-            specialChars[url[i]] += 1
-    
     
 
 #main method to check for a single url or a file of urls
@@ -62,8 +67,7 @@ if __name__ == '__main__':
     inp = input("Type file name with URLs or enter single URL: ")
     if validators.url(inp):
         urlDepth(inp)
-        urlKeyValuePairs(inp)
-        urlSpecialCharacters(inp)
+        fullAnalyzer(inp)
         #put in 1 csv
         matched_data = {'URL Key for ' + inp: urlKey, 'URL Value': urlValue}
         df = pd.DataFrame.from_dict(matched_data)
@@ -74,7 +78,9 @@ if __name__ == '__main__':
             outname = os.path.join(os.path.dirname(sys.executable), outname)
         df.to_csv(outname, encoding='utf-8', header=True, index=False)
         #put other info in text file
-        outname = input + '.txt'
+        if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+            outname = os.path.dirname(sys.executable) + "/" + input + '.txt'
+        else: outname = input + '.txt'
         with open(outname, "w") as file:
             file.write('URL: ' + inp + "\n")
             file.write('Depth: ' + str(len(urls)) + "\n")
@@ -82,11 +88,24 @@ if __name__ == '__main__':
             for u in urls:
                 file.write(' -> ' + u)
             file.write("\n")
-            file.write('Query: ' + query + '\n')
+            #get all the different attributes for url
+            file.write('Scheme: ' + scheme + '\n')
+            if netloc != '':
+                file.write('Netloc: ' + netloc + '\n')
+            if path != '':
+                file.write('Path: ' + path + '\n')
+            if query != '':
+                file.write('Query: ' + query + '\n')
             if fragment_identifier != '':
                 file.write('Fragment Identifier: ' + fragment_identifier + '\n')
-            if port != '':
-                file.write('Port: ' + port + '\n')
+            if username:
+                file.write('Username: ' + username + '\n')
+            if password:
+                file.write('Password: ' + password + '\n')
+            if hostname:
+                file.write('Hostname: ' + hostname + '\n')
+            if port:
+                file.write('Port: ' + port + '\n')   
             file.write('Special Characters:\n')
             for key, value in specialChars.items():
                 if value != 0:
@@ -96,6 +115,7 @@ if __name__ == '__main__':
         folder = input("Enter a folder name to store the data: ")
         if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
             dir = os.path.dirname(sys.executable) + "/" + folder
+            inp = os.path.dirname(sys.executable) + "/" + inp
         else:
             dir = os.getcwd() + "/" + folder
         if not os.path.exists(dir):
@@ -108,8 +128,7 @@ if __name__ == '__main__':
             count += 1
             if validators.url(url):
                 urlDepth(url)
-                urlKeyValuePairs(url)
-                urlSpecialCharacters(url)
+                fullAnalyzer(url)
                 #put in a folder of csv with one common csv for each url
                 matched_data = {'URL Key for ' + url: urlKey, 'URL Value': urlValue}
                 df = pd.DataFrame(matched_data)
@@ -126,24 +145,32 @@ if __name__ == '__main__':
                     for u in urls:
                         file.write(' -> ' + u)
                     file.write("\n")
-                    file.write('Query: ' + query + '\n')
+                    #get all the different attributes for the url
+                    file.write('Scheme: ' + scheme + '\n')
+                    if netloc != '':
+                        file.write('Netloc: ' + netloc + '\n')
+                    if path != '':
+                        file.write('Path: ' + path + '\n')
+                    if query != '':
+                        file.write('Query: ' + query + '\n')
                     if fragment_identifier != '':
                         file.write('Fragment Identifier: ' + fragment_identifier + '\n')
-                    if port != '':
-                        file.write('Port: ' + port + '\n')
+                    if username:
+                        file.write('Username: ' + username + '\n')
+                    if password:
+                        file.write('Password: ' + password + '\n')
+                    if hostname:
+                        file.write('Hostname: ' + hostname + '\n')
+                    if port:
+                        file.write('Port: ' + port + '\n')   
                     file.write('Special Characters:\n')
                     for key, value in specialChars.items():
                         if value != 0:
                             file.write(key + ' -> ' + str(value) + '\n')
                 #reset
-                urls = []
-                urlKey = []
-                urlValue = []
-                urls = []
+                urls, urlKey, urlValue = [], [], []
                 specialChars = {'*' : 0, '\'' : 0, '(' : 0, ')': 0,  ';': 0, ':' : 0,  '@' :0,  '&' : 0,  '=' : 0,  '+': 0,  '$': 0,  ',' : 0,  '/': 0,  '?' : 0,  '%' : 0,  '#' : 0,  '[': 0,  ']': 0}
-                query = 'False'
-                fragment_identifier = ''
-                port = ""
+                query, fragment_identifier, port, scheme, netloc, path, username, password, hostname = '', '', '', '', '', '', '', '', ''
             #url was invalid in the file
             else:
                 outname = 'url-' + str(count) + '.txt'
