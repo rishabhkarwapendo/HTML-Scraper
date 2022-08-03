94bb18304925523e18a467288d62cb2b7fbd169c
Rishabh Karwa <rishabh.karwa@pendo.io>
2022-08-03 12:16:12 -0400

    fixed html calc

--- HTMLSimilarity.py
+++ HTMLSimilarity.py
@@ -4,12 +4,100 @@ The style_similarity method extracts all the CSS classes from both html document
 The structural_similarity uses the difflib.SequenceMatcher method to calculate tag sequence similarity: https://docs.python.org/3/library/difflib.html
 """
 from collections import defaultdict
-from html_similarity import similarity, style_similarity, structural_similarity
+from html_similarity import style_similarity
 import requests as req
 import validators
 import pandas as pd
 import os
 import sys
+import difflib
+from io import StringIO
+import lxml.html
+from parsel import Selector
+
+
+def get_classes(html):
+    doc = Selector(text=html)
+    classes = set(doc.xpath('//*[@class]/@class').extract())
+    result = set()
+    for cls in classes:
+        for _cls in cls.split():
+            result.add(_cls)
+    return result
+
+
+def jaccard_similarity(set1, set2):
+    set1 = set(set1)
+    set2 = set(set2)
+    intersection = len(set1 & set2)
+
+    if len(set1) == 0 and len(set2) == 0:
+        return 1.0
+
+    denominator = len(set1) + len(set2) - intersection
+    return intersection / max(denominator, 0.000001)
+
+
+def style_similarity(page1, page2):
+    """
+    Computes CSS style Similarity between two DOM trees
+
+    A = classes(Document_1)
+    B = classes(Document_2)
+
+    style_similarity = |A & B| / (|A| + |B| - |A & B|)
+
+    :param page1: html of the page1
+    :param page2: html of the page2
+    :return: Number between 0 and 1. If the number is next to 1 the page are really similar.
+    """
+    classes_page1 = get_classes(page1)
+    classes_page2 = get_classes(page2)
+    return jaccard_similarity(classes_page1, classes_page2)
+
+
+def get_tags(doc):
+    '''
+    Get tags from a DOM tree
+
+    :param doc: lxml parsed object
+    :return:
+    '''
+    tags = list()
+
+    for el in doc.getroot().iter():
+        if isinstance(el, lxml.html.HtmlElement):
+            tags.append(el.tag)
+        elif isinstance(el, lxml.html.HtmlComment):
+            tags.append('comment')
+        else:
+            raise ValueError('Don\'t know what to do with element: {}'.format(el))
+
+    return tags
+
+
+def structural_similarity(document_1, document_2):
+    """
+    Computes the structural similarity between two DOM Trees
+    :param document_1: html string
+    :param document_2: html string
+    :return: int
+    """
+    try:
+        document_1 = lxml.html.parse(StringIO(document_1))
+        document_2 = lxml.html.parse(StringIO(document_2))
+    except Exception as e:
+        print(e)
+        return 0
+
+    tags1 = get_tags(document_1)
+    tags2 = get_tags(document_2)
+    diff = difflib.SequenceMatcher()
+    diff.set_seq1(tags1)
+    diff.set_seq2(tags2)
+
+
+    return diff.ratio()
 
 class url_sim:
   def __init__(self, name, style_total, structural_total):
@@ -58,7 +146,7 @@ else:
     with open(html_1, 'r') as file:
         lines = file.readlines()
         #get aggregate similiarity for all urls/html files
-        total = len(lines)
+        total = len(lines) - 1
         for i in range(len(lines)):
             #create a url_sim object 
             u = url_sim(lines[i], 0, 0)
@@ -115,6 +203,6 @@ else:
         joint.append((0.3 * u.structural_total + (1 - 0.3) * u.style_total)/total)
 
     #export to csv file
-    matched_data = {'Name': names, 'CSS Classes Jaccard Similarity': styles, 'HTML Tag Sequence Similarity': structures, 'Joint Similarity': joint}
+    matched_data = {'Name': names, 'CSS Classes Jaccard Similarity': styles, 'HTML Tag Sequence Similarity': structures, 'Joint Similarity: 70% CSS, 30% Tag weightage)': joint}
     df = pd.DataFrame(matched_data)
     df.to_csv(outname, encoding='utf-8', header=True, index=False)
